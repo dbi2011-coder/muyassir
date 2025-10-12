3102a0bde7ce9a013250dc5045b8c7cd4f311848
dbi2011-coder <dbi2011@hotmail.com>
2025-10-12 15:11:33 +0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -5,18 +5,9 @@ class Database {
 
     init() {
         if (!localStorage.getItem('muyassir_initialized')) {
-            // تهيئة قاعدة البيانات بدون حسابات تجريبية
+            // قاعدة بيانات فارغة بدون حسابات مسبقة
             localStorage.setItem('muyassir_users', JSON.stringify([]));
-            localStorage.setItem('muyassir_students', JSON.stringify([]));
-            localStorage.setItem('muyassir_tests', JSON.stringify([]));
-            localStorage.setItem('muyassir_lessons', JSON.stringify([]));
-            localStorage.setItem('muyassir_homework', JSON.stringify([]));
-            localStorage.setItem('muyassir_activities', JSON.stringify([]));
-            localStorage.setItem('muyassir_schedule', JSON.stringify([]));
-            localStorage.setItem('muyassir_handwriting', JSON.stringify([]));
-            localStorage.setItem('muyassir_rewards', JSON.stringify([]));
             localStorage.setItem('muyassir_initialized', 'true');
-            console.log('تم تهيئة قاعدة بيانات فارغة بدون حسابات.');
         }
     }
 
@@ -27,6 +18,10 @@ class Database {
     findUser(username, password, role) {
         const users = this.getUsers();
         return users.find(user => user.username === username && user.password === password && user.role === role);
+    }
+
+    saveUsers(users) {
+        localStorage.setItem('muyassir_users', JSON.stringify(users));
     }
 }
 
